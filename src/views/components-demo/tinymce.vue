14a8ace6afe0c78684d02edd207f6cfdc4281e05
mutouafangzi@163.com <mutouafangzi@163.com>
2018-08-08 19:14:48 +0800

    'tinymce'

--- src/views/components-demo/tinymce.vue
+++ src/views/components-demo/tinymce.vue
@@ -1,23 +1,44 @@
 <!--  -->
 <template>
-  <div></div>
+  <div>
+    <tiny-mce
+    :config           = "Config"
+     v-model          = "Value"
+    :url              = "Url"
+    :max-size         = "MaxSize"
+    :accept           = "Accept"
+    :with-credentials = false
+    @on-ready         = "onEditorReady"
+    @on-destroy       = "onEditorDestroy"
+    @on-upload-success= "onEditorUploadComplete"
+    @on-upload-fail   = "onEditorUploadFail">
+    </tiny-mce>
+  </div>
 </template>
 
 <script>
+
+import TinyMce from '@/components/tinymce/index'
+
 export default {
-  data () {
-    return {
-    };
-  },
-
-  methods: {},
-
-  computed: {},
-
-  components: {},
+    name: "editor-demo",
+    data: function () {
+      return {
+        Config:{
+          theme: 'modern',
+          height: 300
+        },
+        Value:'请输入内容'
+      }
+    },
+    components:{
+      TinyMce
+    }
 }
 
 </script>
-<style scoped>
+<style rel="stylesheet/scss" lang="scss">
+
+  @import "src/styles/mixin.scss";
 
 </style>
